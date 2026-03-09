483a9f0a27d1a2b33496815574778c25ee52c4e7
Link4Electronics <link4electronics@gmail.com>
2026-03-08 21:06:08 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,24 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm sdl2-compat	vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use X64 and ARM64 for the zip links
+	x86_64)  zip_arch=Linux-X64;;
+	aarch64) zip_arch=Linux-ARM64;;
+esac
+ZIP_LINK=$(wget -qO- https://api.github.com/repos/Zelda64Recomp/Zelda64Recomp/releases \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*Zelda64Recompiled.*$zip_arch.zip")
+echo "$ZIP_LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
+wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+bsdtar -xvf /tmp/app.zip -C .
+bsdtar -xvf ./Zelda64Recompiled.tar.gz -C ./AppDir/bin
+wget -q -O ./AppDir/bin/recompcontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
