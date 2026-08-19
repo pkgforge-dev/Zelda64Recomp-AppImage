189d31ed4dd9737027de097d8a00a2efb63c5f83
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:16:40 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,10 +15,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 case "$ARCH" in # they use X64 and ARM64 for the zip links
